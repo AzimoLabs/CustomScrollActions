a4b2bafc8ef2c09d965491f30bfdc0433cbdbf2c
Kamil Krzyk <kamil.krzyk@azimo.com>
2017-05-16 10:59:22 +0200

    project init

---
+++ app/src/main/java/customscrollactions/azimolabs/com/customscrollactions/MainActivity.java
@@ -0,0 +1,13 @@
+package customscrollactions.azimolabs.com.customscrollactions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
